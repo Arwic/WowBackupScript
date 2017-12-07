ecaa1aecbe5a0ade82fa012b8738a4304801c8a0
Tim <tim@tim-ings.com>
2017-12-07 20:57:18 +0800

    Initial commit

---
+++ WowBackup.bat
@@ -0,0 +1,10 @@
+set wowdir="C:\Games\Battle.net\World of Warcraft"
+set gitremote="https://Arwic@github.com/Arwic/WowBackup.git"
+
+cd %wowdir%\WTF
+git initz
+git remote add origin %gitremote%
+git remote set-url origin %gitremote%
+git stage . -A
+git commit -m "Automatic Backup"
+git push origin master
