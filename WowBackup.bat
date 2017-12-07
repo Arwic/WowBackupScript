0fc5e930c2b85c346a7adf2b51b87bc0c59aa405
Tim <tim@tim-ings.com>
2017-12-07 21:29:03 +0800

    Fixes repo initialisation

--- WowBackup.bat
+++ WowBackup.bat
@@ -2,7 +2,7 @@ set wowdir="C:\Games\Battle.net\World of Warcraft"
 set gitremote="https://Arwic@github.com/Arwic/WowBackup.git"
 
 cd %wowdir%\WTF
-git initz
+git init
 git remote add origin %gitremote%
 git remote set-url origin %gitremote%
 git stage . -A
